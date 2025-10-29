a9d1944cb0e9f7be548a37449ae266c897a61cb1
SashaAntoshin <globeinwork@gmail.com>
2025-10-29 19:07:55 +0300

    first review

--- yacut/models.py
+++ yacut/models.py
@@ -3,6 +3,7 @@ from yacut import db
 
 
 class URLMap(db.Model):
+    """Основная модель."""
     id = db.Column(db.Integer, primary_key=True)
     original = db.Column(db.Text, nullable=False)
     short = db.Column(db.String(16), unique=True, nullable=False, index=True)
